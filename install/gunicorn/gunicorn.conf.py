9475c269261cc3646aa8d48c9b543efb7741912c
ramamnsgurijala <rama.gurijala@marks-and-spencer.com>
2019-09-06 04:24:50 +0100

    12th commit

--- install/gunicorn/gunicorn.conf.py
+++ install/gunicorn/gunicorn.conf.py
@@ -1,6 +1,6 @@
 bind = "0.0.0.0:8008"
 logfile = "/var/log/a2billing-flask-api/a2billing_flask_api.log"
-workers = 32
+workers = 2
 daemon = True
 debug = False
 pidfile = "/tmp/gunicorn-a2b-flask-api.pid"
