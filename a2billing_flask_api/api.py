8072ac3c503d0ff15c02bce06de88e94efa19e10
ramamnsgurijala <rama.gurijala@marks-and-spencer.com>
2019-09-21 18:08:52 +0100

    40th commit

--- a2billing_flask_api/api.py
+++ a2billing_flask_api/api.py
@@ -28,9 +28,9 @@ class CardResource(RestResource):
 
 
 # create a special resource for users that excludes email and password
-class UserResource(RestrictOwnerResource):
+class UserResource(RestResource):
     exclude = ('password', 'email',)
-    owner_field = 'user'
+    
 
 
 # class LogrefillResource(RestResource):
