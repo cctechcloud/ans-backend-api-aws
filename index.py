85317a92432ae19c7d933188da8e9d54d75bc900
ramagurijala <grkrishna_mca@yahoo.com>
2020-07-09 16:53:23 +0100

    added index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+import json
+import datetime
+
+
+def handler(event, context):
+    data = {
+        'output': 'Hello World. Adding a2billing.',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
